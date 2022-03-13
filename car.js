9b2b5d6e39babdc3ff6b14f4648d3095dfef657b
DimitriHinds <81481893+DimitriHinds@users.noreply.github.com>
2022-03-13 13:55:48 -0400

    Add files via upload

---
+++ car.js
@@ -0,0 +1,30 @@
+Status = "";
+car_img = "";
+
+function preload(){
+    car_img = loadImage("car.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(car_img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(car_img,0,0,640,350);
+}
